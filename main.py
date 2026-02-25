4e19b17eab80c65863bc80f2844b5ac9e9fcf01c
Lazizxon13 <111843562+Lazizxon13@users.noreply.github.com>
2026-02-25 17:05:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -143,9 +143,3 @@ def webhook():
 def home():
     return "Bot is running", 200
 
-if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url=WEBHOOK_URL + "/" + BOT_TOKEN)
-
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
