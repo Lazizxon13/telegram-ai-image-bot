1a27904ae370b8383e42f7bebf0e1d27ea68da91
Lazizxon13 <111843562+Lazizxon13@users.noreply.github.com>
2026-02-25 23:31:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -27,12 +27,11 @@ def generate_image(message):
     prompt = message.text
 
     try:
-        response = client.images.generate(
+response = client.images.generate(
     model="gpt-image-1",
     prompt=prompt,
     size="1024x1024"
 )
-
         image_base64 = response.data[0].b64_json
         image_bytes = base64.b64decode(image_base64)
 
